825050ae4b0b34c05b8d8cb756848bc4938bee29
tahirahmed1 <50499881+tahirahmed1@users.noreply.github.com>
2019-05-18 15:03:27 +0500

    Add files via upload

---
+++ SimpleCalculator.py
@@ -0,0 +1,7 @@
+# Simple Calculator
+time_kia_hua_hain = int(input('what time is it!'))
+if time_kia_hua_hain <7:
+    print('meet in university cafeteria')
+    print('have some fun')
+else:
+    print('oops got late so meet at nuplex')
